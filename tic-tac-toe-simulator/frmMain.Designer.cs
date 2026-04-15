fe0a5f58b11f8fb762562dabf54e5523d4507183
eliseoluna <eliseo.luna2002@gmail.com>
2026-04-15 14:44:32 -0500

    Add files via upload

---
+++ tic-tac-toe-simulator/frmMain.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Project7
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTopLeft = new System.Windows.Forms.Label();
+            this.lblTopMid = new System.Windows.Forms.Label();
+            this.lblTopRight = new System.Windows.Forms.Label();
+            this.lblMidLeft = new System.Windows.Forms.Label();
+            this.lblMidMid = new System.Windows.Forms.Label();
+            this.lblMidRight = new System.Windows.Forms.Label();
+            this.lblBotMid = new System.Windows.Forms.Label();
+            this.lblBotLeft = new System.Windows.Forms.Label();
+            this.lblBotRight = new System.Windows.Forms.Label();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTopLeft
+            // 
+            this.lblTopLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTopLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopLeft.Location = new System.Drawing.Point(12, 16);
+            this.lblTopLeft.Name = "lblTopLeft";
+            this.lblTopLeft.Size = new System.Drawing.Size(110, 75);
+            this.lblTopLeft.TabIndex = 0;
+            this.lblTopLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTopMid
+            // 
+            this.lblTopMid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTopMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopMid.Location = new System.Drawing.Point(148, 16);
+            this.lblTopMid.Name = "lblTopMid";
+            this.lblTopMid.Size = new System.Drawing.Size(110, 75);
+            this.lblTopMid.TabIndex = 9;
+            this.lblTopMid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTopRight
+            // 
+            this.lblTopRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTopRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopRight.Location = new System.Drawing.Point(281, 16);
+            this.lblTopRight.Name = "lblTopRight";
+            this.lblTopRight.Size = new System.Drawing.Size(110, 75);
+            this.lblTopRight.TabIndex = 10;
+            this.lblTopRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMidLeft
+            // 
+            this.lblMidLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMidLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMidLeft.Location = new System.Drawing.Point(12, 112);
+            this.lblMidLeft.Name = "lblMidLeft";
+            this.lblMidLeft.Size = new System.Drawing.Size(110, 75);
+            this.lblMidLeft.TabIndex = 11;
+            this.lblMidLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMidMid
+            // 
+            this.lblMidMid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMidMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMidMid.Location = new System.Drawing.Point(148, 112);
+            this.lblMidMid.Name = "lblMidMid";
+            this.lblMidMid.Size = new System.Drawing.Size(110, 75);
+            this.lblMidMid.TabIndex = 12;
+            this.lblMidMid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMidRight
+            // 
+            this.lblMidRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMidRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMidRight.Location = new System.Drawing.Point(281, 112);
+            this.lblMidRight.Name = "lblMidRight";
+            this.lblMidRight.Size = new System.Drawing.Size(110, 75);
+            this.lblMidRight.TabIndex = 13;
+            this.lblMidRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBotMid
+            // 
+            this.lblBotMid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBotMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBotMid.Location = new System.Drawing.Point(148, 207);
+            this.lblBotMid.Name = "lblBotMid";
+            this.lblBotMid.Size = new System.Drawing.Size(110, 75);
+            this.lblBotMid.TabIndex = 14;
+            this.lblBotMid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBotLeft
+            // 
+            this.lblBotLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBotLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBotLeft.Location = new System.Drawing.Point(12, 207);
+            this.lblBotLeft.Name = "lblBotLeft";
+            this.lblBotLeft.Size = new System.Drawing.Size(110, 75);
+            this.lblBotLeft.TabIndex = 15;
+            this.lblBotLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBotRight
+            // 
+            this.lblBotRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBotRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBotRight.Location = new System.Drawing.Point(281, 207);
+            this.lblBotRight.Name = "lblBotRight";
+            this.lblBotRight.Size = new System.Drawing.Size(110, 75);
+            this.lblBotRight.TabIndex = 16;
+            this.lblBotRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblWin
+            // 
+            this.lblWin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWin.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblWin.Location = new System.Drawing.Point(12, 298);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(379, 30);
+            this.lblWin.TabIndex = 17;
+            this.lblWin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(12, 341);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(110, 23);
+            this.btnNewGame.TabIndex = 18;
+            this.btnNewGame.Text = "&New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(281, 341);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 23);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPlayer.Location = new System.Drawing.Point(148, 341);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(110, 23);
+            this.lblPlayer.TabIndex = 20;
+            this.lblPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 383);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.lblBotRight);
+            this.Controls.Add(this.lblBotLeft);
+            this.Controls.Add(this.lblBotMid);
+            this.Controls.Add(this.lblMidRight);
+            this.Controls.Add(this.lblMidMid);
+            this.Controls.Add(this.lblMidLeft);
+            this.Controls.Add(this.lblTopRight);
+            this.Controls.Add(this.lblTopMid);
+            this.Controls.Add(this.lblTopLeft);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic-Tac-Toad";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTopLeft;
+        private System.Windows.Forms.Label lblTopMid;
+        private System.Windows.Forms.Label lblTopRight;
+        private System.Windows.Forms.Label lblMidLeft;
+        private System.Windows.Forms.Label lblMidMid;
+        private System.Windows.Forms.Label lblMidRight;
+        private System.Windows.Forms.Label lblBotMid;
+        private System.Windows.Forms.Label lblBotLeft;
+        private System.Windows.Forms.Label lblBotRight;
+        private System.Windows.Forms.Label lblWin;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblPlayer;
+    }
+}
+
---
+++ tic-tac-toe-simulator/frmMain.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project7
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        #region Properties
+        char[,] c2DAns = { {' ', ' ', ' ', },
+                           {' ', ' ', ' ', },
+                           {' ', ' ', ' ', }};
+
+        bool blnGotWinner = false;
+        #endregion
+
+        #region btnNewGame_Click
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            lblWin.Text = string.Empty;
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Label)
+                {
+                    ctrl.ForeColor = System.Drawing.Color.Black;
+                    ctrl.Text = string.Empty;
+                }
+            }
+
+            // Clear the 2D array
+            for (int cols = 0; cols < 3; cols++)
+            {
+                for (int rows = 0; rows < 3; rows++)
+                {
+                    c2DAns[rows, cols] = ' ';
+                }
+            }
+
+            blnGotWinner = false;
+            PlaceChoice(3, 3, 9); // Call method to populate the board for a new game
+        }
+        #endregion
+
+        #region PlaceChoice()
+        public void PlaceChoice(int intMaxRows, int intMaxCols, int intNumTiles)
+        {
+            int intMaxIndex = (intMaxRows * intMaxCols) - 1;
+            int intIndex = 0;
+
+            Random turn = new Random();
+            int number = turn.Next(0, 2); // Toss coin for first turn
+            bool blnPlayer = Convert.ToBoolean(number);
+
+            // Create an array of random and unique numbers within a range
+            var random = new Random();
+            var tempArray = Enumerable.Range(0, 9).OrderBy(t => random.Next()).ToArray();
+            char[] cAnswers = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            // Display who ge the first turn (0 = "O" , 1 = "X")
+            if (blnPlayer)
+                lblPlayer.Text = "X moved first!";
+            else
+                lblPlayer.Text = "O moved first!";
+
+            // Place Players at generated indicies
+            for (int i = 0; i < 9; ++i)
+            {
+                intIndex = tempArray[i];
+                int x = 0;
+                int y = 0;
+
+                // Convert 1D index into 2D indicies
+                x = intIndex / intMaxRows;
+                y = intIndex % intMaxCols;
+
+                if (blnPlayer) // Set character based upon the turn
+                    c2DAns[x, y] = 'X';
+                else
+                    c2DAns[x, y] = 'O';
+
+                if (intIndex == 0)
+                {
+                    if (blnPlayer)
+                    {
+                        lblTopLeft.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblTopLeft.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 1)
+                {
+                    if (blnPlayer)
+                    {
+                        lblTopMid.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblTopMid.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 2)
+                {
+                    if (blnPlayer)
+                    {
+                        lblTopRight.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblTopRight.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 3)
+                {
+                    if (blnPlayer)
+                    {
+                        lblMidLeft.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblMidLeft.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 4)
+                {
+                    if (blnPlayer)
+                    {
+                        lblMidMid.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblMidMid.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 5)
+                {
+                    if (blnPlayer)
+                    {
+                        lblMidRight.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblMidRight.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 6)
+                {
+                    if (blnPlayer)
+                    {
+                        lblBotLeft.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblBotLeft.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else if (intIndex == 7)
+                {
+                    if (blnPlayer)
+                    {
+                        lblBotMid.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblBotMid.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                else
+                {
+                    if (blnPlayer)
+                    {
+                        lblBotRight.Text = "X";
+                        cAnswers[i] = 'X';
+                    }
+                    else
+                    {
+                        lblBotRight.Text = "O";
+                        cAnswers[i] = 'O';
+                    }
+                }
+                blnPlayer = !blnPlayer;
+
+                blnGotWinner = CheckWinner();
+                if (blnGotWinner)
+                    break;
+            }
+            if (blnGotWinner == false)
+            {
+                lblWin.Text = "Tie game!";
+                foreach (Control ctrl in this.Controls)
+                {
+                    if (ctrl is Label && ctrl.Name != "lblWin" && ctrl.Name != "lblPlayer")
+                    {
+                        ctrl.ForeColor = System.Drawing.Color.Coral;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region CheckWinner()
+        private bool CheckWinner() // Sets the appropriate colors calc winner
+        {
+            int intXCount = 0;
+            int intOCount = 0;
+
+            // Check rows
+            for (int rows = 0; rows < 3; rows++)
+            {
+                intXCount = 0;
+                intOCount = 0;
+                for (int cols = 0; cols < 3; cols++)
+                {
+                    if (c2DAns[rows, cols] == 'X')
+                        intXCount++;
+                    else if (c2DAns[rows, cols] == 'O')
+                        intOCount++;
+                }
+                if (intXCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "X wins!";
+                    if (rows == 0)
+                    {
+                        lblTopLeft.ForeColor = System.Drawing.Color.Red;
+                        lblTopMid.ForeColor = System.Drawing.Color.Red;
+                        lblTopRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (rows == 1)
+                    {
+                        lblMidLeft.ForeColor = System.Drawing.Color.Red;
+                        lblMidMid.ForeColor = System.Drawing.Color.Red;
+                        lblMidRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (rows == 2)
+                    {
+                        lblBotLeft.ForeColor = System.Drawing.Color.Red;
+                        lblBotMid.ForeColor = System.Drawing.Color.Red;
+                        lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    blnGotWinner = true;
+                }
+                if (intOCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "O wins!";
+                    if (rows == 0)
+                    {
+                        lblTopLeft.ForeColor = System.Drawing.Color.Red;
+                        lblTopMid.ForeColor = System.Drawing.Color.Red;
+                        lblTopRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (rows == 1)
+                    {
+                        lblMidLeft.ForeColor = System.Drawing.Color.Red;
+                        lblMidMid.ForeColor = System.Drawing.Color.Red;
+                        lblMidRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (rows == 2)
+                    {
+                        lblBotLeft.ForeColor = System.Drawing.Color.Red;
+                        lblBotMid.ForeColor = System.Drawing.Color.Red;
+                        lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    blnGotWinner = true;
+                }
+            }
+            // Check cols
+            for (int cols = 0; cols < 3; cols++)
+            {
+                intXCount = 0;
+                intOCount = 0;
+                for (int rows = 0; rows < 3; rows++)
+                {
+                    if (c2DAns[rows, cols] == 'X')
+                        intXCount++;
+                    else if (c2DAns[rows, cols] == 'O')
+                        intOCount++;
+                }
+                if (intXCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "X wins!";
+                    if (cols == 0)
+                    {
+                        lblTopLeft.ForeColor = System.Drawing.Color.Red;
+                        lblMidLeft.ForeColor = System.Drawing.Color.Red;
+                        lblBotLeft.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (cols == 1)
+                    {
+                        lblTopRight.ForeColor = System.Drawing.Color.Red;
+                        lblMidRight.ForeColor = System.Drawing.Color.Red;
+                        lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (cols == 2)
+                    {
+                        lblTopRight.ForeColor = System.Drawing.Color.Red;
+                        lblMidRight.ForeColor = System.Drawing.Color.Red;
+                        lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                        blnGotWinner = true;
+                }
+                if (intOCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "O wins!";
+                    if (cols == 0)
+                    {
+                        lblTopLeft.ForeColor = System.Drawing.Color.Red;
+                        lblMidLeft.ForeColor = System.Drawing.Color.Red;
+                        lblBotLeft.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (cols == 1)
+                    {
+                        lblTopMid.ForeColor = System.Drawing.Color.Red;
+                        lblMidMid.ForeColor = System.Drawing.Color.Red;
+                        lblBotMid.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else if (cols == 2)
+                    {
+                        lblTopRight.ForeColor = System.Drawing.Color.Red;
+                        lblMidRight.ForeColor = System.Drawing.Color.Red;
+                        lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    }
+                    blnGotWinner = true;
+                }
+            }
+
+            // Check right diagonal
+            intXCount = 0;
+            intOCount = 0;
+
+            if (blnGotWinner == false)
+            {
+                if (c2DAns[0, 0] == 'X')
+                    intXCount++;
+                else if (c2DAns[0, 0] == 'O')
+                    intOCount++;
+                if (c2DAns[1, 1] == 'X')
+                    intXCount++;
+                else if (c2DAns[1, 1] == 'O')
+                    intOCount++;
+                if (c2DAns[2, 2] == 'X')
+                    intXCount++;
+                else if (c2DAns[2, 2] == 'O')
+                    intOCount++;
+                if (intXCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "X wins!";
+                    lblTopLeft.ForeColor = System.Drawing.Color.Red;
+                    lblMidMid.ForeColor = System.Drawing.Color.Red;
+                    lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    blnGotWinner = true;
+
+                }
+                if (intOCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "O wins!";
+                    lblTopLeft.ForeColor = System.Drawing.Color.Red;
+                    lblMidMid.ForeColor = System.Drawing.Color.Red;
+                    lblBotRight.ForeColor = System.Drawing.Color.Red;
+                    blnGotWinner = true;
+
+                }
+            }
+            // Check left diagonal
+            intXCount = 0;
+            intOCount = 0;
+
+            if (blnGotWinner == false)
+            {
+                if (c2DAns[2, 0] == 'X')
+                    intXCount++;
+                else if (c2DAns[2, 0] == 'O')
+                    intOCount++;
+                if (c2DAns[1, 1] == 'X')
+                    intXCount++;
+                else if (c2DAns[1, 1] == 'O')
+                    intOCount++;
+                if (c2DAns[0, 2] == 'X')
+                    intXCount++;
+                else if (c2DAns[0, 2] == 'O')
+                    intOCount++;
+                if (intXCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "X wins!";
+                    lblTopRight.ForeColor = System.Drawing.Color.Red;
+                    lblMidMid.ForeColor = System.Drawing.Color.Red;
+                    lblBotLeft.ForeColor = System.Drawing.Color.Red;
+                    blnGotWinner = true;
+
+                }
+                if (intOCount == 3 && blnGotWinner == false)
+                {
+                    lblWin.Text = "O wins!";
+                    lblTopRight.ForeColor = System.Drawing.Color.Red;
+                    lblMidMid.ForeColor = System.Drawing.Color.Red;
+                    lblBotLeft.ForeColor = System.Drawing.Color.Red;
+                    blnGotWinner = true;
+
+                }
+            }
+
+            if (blnGotWinner)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+
+        #region btnExit_Click
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
